3bb8fea1cdda62b22d4905d1d44031841ec0d593
Sahar <100313500+SaharDeldar@users.noreply.github.com>
2022-07-16 21:34:08 +0430

    Add files via upload

---
+++ mul.py
@@ -0,0 +1,18 @@
+a=int(input("a "))
+b=int(input("b  "))
+print ("x", end="")
+x=1
+while x<=a:
+    print(x,end="")
+    x+=1
+y=1
+while y<=b:
+      print(" ")
+      print(y,end="")   
+      z=1    
+      while z<=a:
+            print(y*z,end="")
+            z+=1
+      y+=1
+print()    
+
